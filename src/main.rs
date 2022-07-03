be80d9313c56f3c24847c08c0a1e2d6e9f448769
kunevi <kuniewskiw@gmail.com>
2022-07-03 16:30:33 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use tokio::{
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     println!("Listening on {}", listener.local_addr().unwrap());
     let (tx, _rx) = broadcast::channel(10);
 
